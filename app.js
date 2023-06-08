1cc670d6338ea38861d4d8bbc5067ad46a51c74d
Vishwajeetsinh Rathod <rathodvishwajeet64@gmail.com>
2023-06-08 11:15:21 +0530

    day3

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
         import React from "react"
         import ReactDOM from "react-dom/client"
 
-        const heading = React.createElement('h1',{
-            id:'rooting', 
-            className: 'vish'
-        },'Vishwajeet from react parcel is cool')
+        const Title = () => (
+        <h1 id="name" key="naming">
+            vishwajeet
+        </h1>
+        );
+        
+        //! Components 
+        
+        let xyz = 1
+        /* way one of writing react component */
+        const HeaderComponenet = () => { 
+            return (
+                <div>
+                    <Title/>
+                    <Title/>
+                </div>
+            );
+        };
 
-        const heading2 = React.createElement('h2', {
-            id: 'heading2'
-        },
-        'This is heading 2')
+        /* Way two of writing react component*/
+            const HeaderComponenet2 = () => (
+                    <div>
+                        <Title/>                   
+                    </div>
+                );
+        
 
-        const container = React.createElement('div', {
-            id: 'container'
-        },
-        [heading, heading2])
-        console.log(heading)
         const root = ReactDOM.createRoot(document.getElementById('root'))
+
         //passing a react element inside the root. 
-        root.render(container)
+        root.render(<HeaderComponenet/>)
